0f7afa8e4437ca56545a546fe3bac2d312b62224
gajjar96019 <gajjar96019@gmail.com>
2024-03-19 15:35:09 +0530

    responsive

---
+++ JS/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items:1,
+        autoplay:true,
+        animateOut:'fadeOut',
+        // navText:['<i class="fa-solid fa-arrow-left"></i>','<i class="fa-solid fa-arrow-right"></i>']
+    });
+});
